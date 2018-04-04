eaff86d055867bce6a98d1ab090aeca1dfbc7530
karolinasuchecka <suchecka.karolina@gmail.com>
2018-04-04 15:19:31 +0200

    fichiers texte pour la comparaison structurée

---
+++ traite_fichiers.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $dirF = '../F';
+my $dirFC = '../FC';
+
+my @filesF = `ls $dirF/*xml`;
+my @filesFC = `ls $dirFC/*xml`;
+
+foreach my $file (@filesF) {
+
+	chop $file;
+
+	my $txtfile = $file;
+	$txtfile =~ s/\.xml$/.txt/;
+	$txtfile =~ s/\.\.\/F/Medite-F-txt/;
+
+	my $cmd = "xsltproc xml2txtMedite_structure.xsl $file > $txtfile";
+	`$cmd`;
+}
+
+foreach my $file (@filesFC) {
+
+	chop $file;
+
+	my $txtfile = $file;
+	$txtfile =~ s/\.xml$/.txt/;
+	$txtfile =~ s/\.\.\/FC/Medite-FC-txt/;
+
+	my $cmd = "xsltproc xml2txtMedite_structure.xsl $file > $txtfile";
+	`$cmd`;
+}
